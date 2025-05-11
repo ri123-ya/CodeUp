7d9c4ab76a963e717e81bb10fbc294a089ea140f
Riya Rastogi <riya02rastogi@gmail.com>
2025-05-11 11:01:26 +0530

    handle Clerk user creation events and sync user data

--- convex/http.ts
+++ convex/http.ts
@@ -7,6 +7,7 @@ import { api } from "./_generated/api";
 const http = httpRouter();
 
 http.route({
+    
     path: "/clerk-webhook",
     method: "POST",
     handler: httpAction(async (ctx, request) => {
